be0901a10b837f0e4328923381d9e840025de28a
MathMarcondes <matheus.marcondes@outlook.com>
2024-09-25 20:57:31 -0300

    Adicionando instalação de react-native-geocoding, react-native-google-maps-directions, react-native-maps, react-native-maps-directions

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -17,7 +17,7 @@ export default function TabLayout() {
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Home',
+          title: 'Início',
           tabBarIcon: ({ color, focused }) => (
             <TabBarIcon name={focused ? 'home' : 'home-outline'} color={color} />
           ),
